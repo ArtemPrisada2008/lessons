1e8f4311c0354803258285c5a8444d2a21728af8
ArtemPrisada <artemprisada10@gmail.com>
2023-09-27 21:18:10 +0300

    lesson1

--- Homework1.java
+++ Homework1.java
@@ -2,11 +2,10 @@ package lesson1;
 
 import java.util.Random;
 
-public class dz2 {
+public class Homework1 {
     public static void main(String[] args) {
         Random random = new Random();
-
-        int random_number = random.nextInt(20) -20;
+        int random_number = random.nextInt(20) - 20;
         System.out.println(random_number);
     }
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.println("Artem");
+        }
     }
 }
-    }
